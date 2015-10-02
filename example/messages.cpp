796fc0dfaefa8ad5fc5c7c6e799414d0c346ec4e
Pavel Yevtukhov <pavel@tundramobile.com>
2015-10-02 12:44:56 +0300

    guard delay is unneded anymore

--- example/messages.cpp
+++ example/messages.cpp
@@ -9,25 +9,22 @@ template <typename T>
 static void printError(Textmagic::BaseController<T>& controller);
 static void printSendResult(Textmagic::SendResultModel& sendResult);
 static void printMessage(const Textmagic::MessageModel& message);
-static void guardDelay();
 
 int main(int argc, char* argv[]){
+	std::cout << "********* MessagesController example *********" << std::endl;
 	if (argc < 3) {
 		std::cout << "********* Please enter valid login & api key *********" << std::endl;
 		std::cout << ">> bin/messages_example username C7XDKZOQZo6HvhJPtO0MBcWl3qwtp2" << std::endl;
 		return 1;
 	}
 	
-	std::cout << "********* MessagesController example *********" << std::endl;
 	Rest::RequestData vars;
 	Client tm(argv[1], argv[2]);
-	
 	
 	//********************* list example ***********************
 	std::cout << "********* MessagesController.list() *********" << std::endl;
 	
 	Resources<MessageModel> messageList  = tm.Messages().list();
-	
 	if (tm.Messages().isError) {
 		printError(tm.Messages());
 		return 1;
@@ -40,10 +37,10 @@ int main(int argc, char* argv[]){
 			printMessage(messageList.resources[i]);
 		}
 	}
-	guardDelay();
 	
 	
 	std::cout << "********* MessagesController get list of messages *********" << std::endl;
+	
 	if (messageList.resources.empty()) {
 		std::cout << "no messages" << std::endl;
 	} else {
@@ -54,12 +51,10 @@ int main(int argc, char* argv[]){
 			printMessage(message);
 		}
 	}
-	guardDelay();
 	
-		std::cout << "********* Obtain price *********" << std::endl;
-	const std::string phoneNumber = "999"; // free number
-	//const std::string phoneNumber = "+380660000000"; // some real number
+	std::cout << "********* Obtain price *********" << std::endl;
 	
+	const std::string phoneNumber = "999"; // 999 - is special number for testing (free)
 	Textmagic::PriceResultModel priceResult = tm.Messages().price(phoneNumber, "The simple fake message");
 	if (tm.Messages().isError){
 		printError(tm.Messages());
@@ -69,10 +64,6 @@ int main(int argc, char* argv[]){
 		<< priceResult.getPriceForCountry("")
 		<< std::endl;
 	}
-	
-	guardDelay();
-	
-	
 	
 	//******************* Send example ***********************
 	std::cout << "********* MessagesController.send() *********" << std::endl;
@@ -84,9 +75,6 @@ int main(int argc, char* argv[]){
 		printSendResult(result);
 	}
 	
-	guardDelay();
-	
-	
 	//******************* Get example ***********************
 	std::cout << "********* MessagesController.get() *********" << std::endl;
 	
@@ -96,15 +84,14 @@ int main(int argc, char* argv[]){
 	} else {
 		printMessage(message);
 	}
-	guardDelay();
 	
 	//******************* Remove example ***********************
 	std::cout << "********* MessagesController.remove() *********" << std::endl;
 	bool rresult = tm.Messages().remove(message.id);
 	std::cout  << "Delete Result: " << (rresult ? "Ok" : "Fail") << std::endl;
 	
-	return 0;
 	
+	return 0;
 }
 
 
@@ -137,10 +124,4 @@ static void printMessage(const Textmagic::MessageModel& message)
 	<< message.status  << " * "
 	<< message.text  << " * "
 	<< std::endl;
-}
-
-static void guardDelay()
-{
-	const std::chrono::duration<double, std::milli> delay(1000);
-	std::this_thread::sleep_for(delay);
 }
